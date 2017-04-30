a25a909a166f21b919a852114a6378b7bf1a0325
Darien Keyack <keyacd@rpi.edu>
2017-04-30 02:33:24 +0000

    Test Commit

--- Project 2/main.c
+++ Project 2/main.c
@@ -1,8 +1,8 @@
 #define _POSIX_SOURCE
 #define _SVID_SOURCE
+
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <netinet/in.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -15,16 +15,20 @@
 #include <errno.h>
 #include <dirent.h>
 
-struct Process {
-    char id;
-    int memory;
-    int * arrive_times;
-    int * run_times; //This and arrive_times should be the same, so only need one variable to track their sizes
-    int list_size;
-    
-};
+#include "process.h"
 
-int get_parts(char **memory, int **partitions){
+// Darien Keyack (661190088) and Corwin Aucoin (661178786)
+
+int next_fit(int part_num, int** partitions, char** memory, struct Process proc) {
+  int zz = 0;
+  for(zz = 0; zz < part_num; zz++) {
+    if(partitions[zz][2] >= proc.memory) {
+   
+    }
+  }
+}
+
+int get_parts(char **memory, int **partitions) {
   int i, j, count, parts;
   count = 0;
   parts = 0;
@@ -45,7 +49,45 @@ int get_parts(char **memory, int **partitions){
   return parts;
 }
 
-int main(int argc, char * argv[]){
+int main(int argc, char * argv[]) {
+  // Open input file
+  if (argc < 2) msg_error("argc < 2");
+  FILE *input = fopen(argv[1], "r");
+  if (input == NULL) msg_error("failed to open input file");
+  
+  // Read number of processes from input file
+  char line[256];
+  fgets(line, sizeof(line), input);
+  if (line == NULL) msg_error("input file too short");
+  int num_proc = atoi(line);
+  if (num_proc < 1 || num_proc > 26) msg_error("invalid number of processes");
+  
+  // Read in raw processes from input file
+  char** proc_raw = (char**) calloc(num_proc, 256);
+  int i = 0;
+  while (fgets(line, sizeof(line), input) && i < num_proc) {
+      if (line[0] != ' ' && line[0] != '#') {
+          proc_raw[i] = line;
+          i++;
+      }
+  }
+  fclose(input);
+  if (i < num_proc || fgets(line, sizeof(line), input) != NULL) msg_error("invalid number of processes");
+  
+  // Create processes from raw data
+  struct Process* proc_array = (struct Process*) calloc(num_proc, sizeof(struct Process));
+  int j;
+  int init;
+  for (i = 0; i < num_proc; i++) {
+      proc_array[i].id = proc_raw[i][0];
+      proc_array[i].mem_frames = get_seg(2, 256, proc_raw[i], ' ');
+      j = 2 + 1 + int_len(proc_array[i].mem_frames);
+      proc_array[i].t_arr = get_seg(j, 256, proc_raw[i], '/');
+      j += 1 + int_len(proc_array[i].t_arr);
+      proc_array[i].t_run = get_seg(j, 256, proc_raw[i], ' ');
+      j += 1 + int_len(proc_array[i].t_run);
+  }
+    
   char memory[32][8];
   return EXIT_SUCCESS;
 }
--- Project 2/process.h
+++ Project 2/process.h
@@ -1,12 +1,11 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-
-#include "p2-process.h"
-
-// Darien Keyack (661190088) and Corwin Aucoin (661178786)
+struct Process {
+    char id;
+    int memory;
+    int* arrive_times;
+    int* run_times; // This and arrive_times should be the same, so only need one variable to track their sizes
+    int list_size;
+    
+};
 
 // Get segment (up to forbidden char) of provided msg
 int get_seg(int init, int max, char* msg, char forbidden) {
@@ -29,46 +28,4 @@ int int_len(int i) {
 void msg_error(char *msg) {
     fprintf(stderr, "ERROR: <%s>\n", msg);
     exit(EXIT_FAILURE);
-}
-
-int main(int argc, char *argv[]) {
-    // Open input file
-    if (argc < 2) msg_error("argc < 2");
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL) msg_error("failed to open input file");
-    
-   // Read number of processes from input file
-    char line[256];
-    fgets(line, sizeof(line), input);
-    if (line == NULL) msg_error("input file too short");
-    int num_proc = atoi(line);
-    if (num_proc < 1 || num_proc > 26) msg_error("invalid number of processes");
-    
-   // Read in raw processes from input file
-    char** proc_raw = (char**) calloc(num_proc, 256);
-    int i = 0;
-    while (fgets(line, sizeof(line), input) && i < num_proc) {
-        if (line[0] != ' ' && line[0] != '#') {
-            proc_raw[i] = line;
-            i++;
-        }
-    }
-    fclose(input);
-    if (i < num_proc || fgets(line, sizeof(line), input) != NULL) msg_error("invalid number of processes");
-    
-   // Create processes from raw data
-    struct Process* proc_array = (struct Process*) calloc(num_proc, sizeof(struct Process));
-    int j;
-    int init;
-    for (i = 0; i < num_proc; i++) {
-        proc_array[i].id = proc_raw[i][0];
-        proc_array[i].mem_frames = get_seg(2, 256, proc_raw[i], ' ');
-        j = 2 + 1 + int_len(proc_array[i].mem_frames);
-        proc_array[i].t_arr = get_seg(j, 256, proc_raw[i], '/');
-        j += 1 + int_len(proc_array[i].t_arr);
-        proc_array[i].t_run = get_seg(j, 256, proc_raw[i], ' ');
-        j += 1 + int_len(proc_array[i].t_run);
-    }
-    
-   exit(EXIT_SUCCESS);
 }
